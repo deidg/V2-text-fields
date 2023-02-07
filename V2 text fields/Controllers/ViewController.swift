d745405dd1eb18eeb7fbd383ba2f6d4d97169781
deidg <64797316+deidg@users.noreply.github.com>
2023-02-07 10:31:02 +0300

    initial commit

---
+++ V2 text fields/Controllers/ViewController.swift
@@ -0,0 +1,95 @@
+//
+//  ViewController.swift
+//  V2 text fields
+//
+//  Created by Alex on 07.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    let Title: UILabel = {
+        let titleLabel = UILabel()
+        
+        
+        return titleLabel
+    }()
+    
+    let noDigitLabel: UILabel = {
+        let noDigitLabel = UILabel()
+        
+        
+        return noDigitLabel
+    }()
+    let lettersTextField: UITextField = {
+        let lettersTextField = UITextField()
+        
+        
+        return lettersTextField
+    }()
+    
+    let inputLimitLabel: UILabel = {
+        let inputLimitLabel = UILabel()
+        
+        
+        return inputLimitLabel
+    }()
+    let limitTextField: UITextField = {
+        let limitTextField = UITextField()
+        
+        
+        return limitTextField
+    }()
+    
+    let onlyCharectersLabel = {
+        let onlyCharectersLabel = UILabel()
+        
+        
+        return onlyCharectersLabel
+    }()
+    let characterField = {
+        let characterField = UITextField()
+        
+        
+        return characterField
+    }()
+    
+    let linkLabel: UILabel = {
+        let linkLabel = UILabel()
+        
+        
+        return linkLabel
+    }()
+    let linkTextField: UITextField = {
+        let linkTextField = UITextField()
+        
+        
+        return linkTextField
+    }()
+    
+    let validationLabel: UILabel = {
+        let validationLabel = UILabel()
+        
+        
+        return validationLabel
+    }()
+    let passwordTextField: UITextField = {
+        let passwordTextField = UITextField()
+        
+        
+        return passwordTextField
+    }()
+    let validationRulesLabel: UILabel = {
+        let validationRulesLabel = UILabel()
+        
+        
+        return validationRulesLabel
+    }()
+    
+}
+
